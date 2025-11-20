1538440d27b9681a5595b012675f588a27f185fb
YuYusuki <82035555+YuYusuki@users.noreply.github.com>
2025-11-20 17:34:09 +0000

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ const App = () => (
                 {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                 <Route path="*" element={<NotFound />} />
               </Routes>
-              
+              <Index />
             </main>
             <Footer />
           </div>
